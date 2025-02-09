6a363729649ad4250ce37d0533370a2f6d40d0b0
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-02-09 20:27:57 +0530

    'Update'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class Main {
+
+
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+
+            String url="jdbc:mysql://localhost:3306/students";
+            String username="root";
+            String password="2121912";
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                System.out.println("Drivers loaded successfully");
+            }catch(ClassNotFoundException e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+
+            try{
+                Connection connection= DriverManager.getConnection(url,username,password);
+                System.out.println("Connection established");
+            }catch(SQLException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
 }
